18a2783252232624b7cae9206e48403df0137643
Belagir <belagir.tedab@gmail.com>
2024-03-01 15:37:52 +0100

    command system starting point

--- inc/tarasque.h
+++ inc/tarasque.h
@@ -2,9 +2,40 @@
 #ifndef __TARASQUE_H__
 #define __TARASQUE_H__
 
+#include <stddef.h>
+
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+
+
 typedef struct tarasque_engine tarasque_engine;
+
+// -------------------------------------------------------------------------------------------------
+
+typedef struct entity_template {
+    size_t data_size;
+    void *data;
+
+    void (*on_init)(void *data, tarasque_engine *handle);
+    void (*on_deinit)(void *data, tarasque_engine *handle);
+    void (*on_frame)(void *data, float elapsed_ms, tarasque_engine *handle);
+} entity_template;
+
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
 
 tarasque_engine *tarasque_engine_create(void);
 void tarasque_engine_destroy(tarasque_engine **handle);
+
+// -------------------------------------------------------------------------------------------------
+
+void tarasque_engine_run(tarasque_engine *handle, int fps);
+void tarasque_engine_quit(tarasque_engine *handle);
+
+// -------------------------------------------------------------------------------------------------
+
+void tarasque_engine_add_entity(tarasque_engine *handle, char *str_path, char *str_id, entity_template template);
 
 #endif
---
+++ src/command/command.h
@@ -0,0 +1,47 @@
+
+#ifndef __COMMAND_H__
+#define __COMMAND_H__
+
+#include <ustd/allocation.h>
+#include <ustd/range.h>
+
+#include <tarasque.h>
+
+typedef struct command_queue command_queue;
+
+typedef enum command_flavor {
+    INVALID,
+    ADD_ENTITY,
+    REMOVE_ENTITY,
+    SUBSCRIBE_TO_EVENT,
+} command_flavor;
+
+typedef struct command_remove_entity {
+    range(u8) *id_path;
+} command_remove_entity;
+
+typedef struct command_add_entity {
+    range(u8) *id_path;
+    range(u8) *id;
+    //TODO : entity_template_copy template;
+} command_add_entity;
+
+typedef struct command_subscribe_to_event {
+    range(u8) *target_event_name;
+    // TODO : entity *subscribed;
+    void (*callback)(void *entity_data, void *event_data);
+} command_subscribe_to_event;
+
+typedef struct command {
+    command_flavor flavor;
+    // TODO : entity *source;
+} command;
+
+command command_create_add_entity(char *id_path, char *id, entity_template template, allocator alloc);
+command command_create_remove_entity(char *id_path, allocator alloc);
+command command_create_subscribe_to_event(char *id_path, allocator alloc);
+
+void command_destroy(command *cmd, allocator alloc);
+
+
+#endif
