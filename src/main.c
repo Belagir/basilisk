9c40eef68239d25256f60dac8d7c73153c638b50
Belagir <belagir.tedab@gmail.com>
2024-03-03 15:26:32 +0100

    entity stepping

--- src/main.c
+++ src/main.c
@@ -11,24 +11,31 @@ static void dummy_entity_deinit(void *data, tarasque_engine *handle) {
     printf("dummy n°%d deinitialized.\n", *(int *) data);
 }
 
+static void dummy_entity_step(void *data, float elapsed_ms, tarasque_engine *handle) {
+    printf("dummy n°%d is doing something...\n", *(int *) data);
+}
+
 static void dummy_setup(tarasque_engine *handle) {
     tarasque_engine_add_entity(handle, "", "1", (entity_template) { 
-            .data = &(int) { 1 },
+            .data      = &(int) { 1 },
             .data_size = sizeof(int),
-            .on_init = &dummy_entity_init,
+            .on_init   = &dummy_entity_init,
             .on_deinit = &dummy_entity_deinit,
+            .on_frame  = &dummy_entity_step
     });
     tarasque_engine_add_entity(handle, "1", "2", (entity_template) { 
-            .data = &(int) { 2 },
+            .data      = &(int) { 2 },
             .data_size = sizeof(int),
-            .on_init = &dummy_entity_init,
+            .on_init   = &dummy_entity_init,
             .on_deinit = &dummy_entity_deinit,
+            .on_frame  = &dummy_entity_step
     });
     tarasque_engine_add_entity(handle, "1/2", "3", (entity_template) { 
-            .data = &(int) { 3 },
+            .data      = &(int) { 3 },
             .data_size = sizeof(int),
-            .on_init = &dummy_entity_init,
+            .on_init   = &dummy_entity_init,
             .on_deinit = &dummy_entity_deinit,
+            .on_frame  = &dummy_entity_step
     });
 }
 
