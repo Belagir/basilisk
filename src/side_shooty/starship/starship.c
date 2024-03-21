9435107dbe6050dbbaee146d33b59a7b4db52e38
Belagir <belagir.tedab@gmail.com>
2024-03-21 11:02:37 +0100

    depending on sdl_image headers to load sprites

--- src/side_shooty/starship/starship.c
+++ src/side_shooty/starship/starship.c
@@ -1,20 +1,20 @@
 
+
+#include <SDL2/SDL_image.h>
+
 #include <ustd/res.h>
 
 #include "starship.h"
 
-// DECLARE_RES(ship_image, "res_ship_bmp")
-
 extern const unsigned char _binary_res_ship_bmp_start[];
 extern const unsigned char _binary_res_ship_bmp_end[];
-extern const unsigned long _binary_res_ship_bmp_size;
 
 static void init(tarasque_entity *entity)
 {
     starship *ship = (starship *) entity;
 
-    ship->sprite = SDL_CreateRGBSurfaceFrom((void *) _binary_res_ship_bmp_start, 64, 64, 32, 4, 0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF);
-    printf("%#010x\n", ship->sprite);
+    ship->sprite = IMG_Load_RW(SDL_RWFromConstMem(_binary_res_ship_bmp_start, (int) ((size_t) _binary_res_ship_bmp_end - (size_t) _binary_res_ship_bmp_start)), 0);
+
 }
 
 static void deinit(tarasque_entity *entity)
