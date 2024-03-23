eaacc2b6f8f49c11041b22c5f24f02bb736f3b0a
Belagir <belagir.tedab@gmail.com>
2024-03-23 11:39:33 +0100

    documentation for sdl window graft

--- src/grafts/sdl_window/sdl_window.c
+++ src/grafts/sdl_window/sdl_window.c
@@ -11,23 +11,23 @@ void graft_sdl_window(tarasque_entity *entity, void *graft_args)
 
     const graft_sdl_window_args sdl_win_args = *(const graft_sdl_window_args *) graft_args;
 
-    tarasque_entity_add_child(entity, "", "SDL Context",
+    tarasque_entity_add_child(entity, "", "Context",
             be_context_sdl_entity());
 
-    tarasque_entity_add_child(entity, "SDL Context", "SDL Window",
+    tarasque_entity_add_child(entity, "Context", "Window",
             be_window_sdl_entity(&(be_window_sdl) {
                     .title = sdl_win_args.for_window.title,
                     .x = sdl_win_args.for_window.x, .y = sdl_win_args.for_window.y,
                     .w = sdl_win_args.for_window.w, .h = sdl_win_args.for_window.h,
                     .flags = sdl_win_args.for_window.flags }));
 
-    tarasque_entity_add_child(entity, "SDL Context/SDL Window", "SDL Render Manager",
+    tarasque_entity_add_child(entity, "Context/Window", "Render Manager",
             be_render_manager_sdl_entity(&(be_render_manager_sdl) {
                     .clear_color = sdl_win_args.for_renderer.clear_color,
                     .flags = sdl_win_args.for_renderer.flags,
-                    .window_entity_name = "SDL Window" }));
+                    .window_entity_name = "Window" }));
 
-    tarasque_entity_add_child(entity, "SDL Context/SDL Window", "SDL Event Relay",
+    tarasque_entity_add_child(entity, "Context/Window", "Event Relay",
             be_event_relay_sdl_entity(&(be_event_relay_sdl) { 0u }));
 }
 
