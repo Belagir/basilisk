306ad4182ce022f4ef73782e4a179c31b3882caf
Belagir <belagir.tedab@gmail.com>
2024-03-25 13:24:26 +0100

    merging from side shooty

--- src/main.c
+++ src/main.c
@@ -1,21 +1,12 @@
 
 #include <tarasque.h>
 
-#include "side_shooty/side_shooty.h"
-
 // -------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------
 
 int main(void)
 {
-    tarasque_engine *handle = tarasque_engine_create();
-
-    (void) tarasque_entity_add_child(tarasque_engine_root_entity(handle), "Game", side_shooty_game());
-
-    tarasque_engine_run(handle, 60);
-
-    tarasque_engine_destroy(&handle);
 
     return 0;
 }
--- src/side_shooty/side_shooty.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef __SIDE_SHOOTY_H__
-#define __SIDE_SHOOTY_H__
-
-#include <tarasque.h>
-
-tarasque_specific_entity side_shooty_game(void);
-
-#endif
