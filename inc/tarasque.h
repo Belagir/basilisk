b15249ed67ad79444a69357fa522bad87c4b7343
Belagir <belagir.tedab@gmail.com>
2024-03-26 09:27:51 +0100

    new main header w/ useful imports

---
+++ inc/tarasque.h
@@ -0,0 +1,12 @@
+
+#ifndef __TARASQUE_H__
+#define __TARASQUE_H__
+
+#include <tarasque_bare.h>
+
+#include <ustd/common.h>
+#include <ustd/allocation.h>
+#include <ustd/range.h>
+#include <ustd/res.h>
+
+#endif
